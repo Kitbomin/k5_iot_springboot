3d6ca9fd3cec7e1d47e13d8e98486d332b940758
Kitbomin <zzxxcc1433@naver.com>
2025-08-11 12:45:31 +0900

    서블릿 및 스트링부트 동작 구조

---
+++ query.sql
@@ -0,0 +1,13 @@
+
+# 1. 스키마 생성 
+Drop database if exists k5_iot_springboot;
+
+# 2. 스키마 생성 + 문자셋 / 정렬 설정
+create database if not exists k5_iot_springboot
+	character set utf8mb4
+    collate utf8mb4_general_ci;
+    
+# 3. 스키마 선택
+use k5_iot_springboot;
+
+
