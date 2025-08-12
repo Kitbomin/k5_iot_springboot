63fb2cbfcfb879b552696964139ac221876eeacd
Kitbomin <zzxxcc1433@naver.com>
2025-08-12 12:31:11 +0900

    sql문 추가

--- query.sql
+++ query.sql
@@ -13,8 +13,19 @@ use k5_iot_springboot;
 
 # 0811(A_Test)
 Create table if not exists test(
-	id bigint primary key auto_increment,
+	test_id bigint primary key auto_increment,
     name varchar(50) not null
 );
 
 select * from test;
+
+# 0812(B_Student)
+create table if not exists students(
+	id bigint primary key auto_increment,
+    name varchar(100) not null,
+    email varchar(100) not null unique,
+    unique key uq_name_email (name,email)
+    -- : name+email 조합이 유일하도록 설정 
+);
+
+select * from students;
