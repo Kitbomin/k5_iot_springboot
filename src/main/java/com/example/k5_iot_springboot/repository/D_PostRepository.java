545a664ebd8b26ea686cb0eb8d383797296ef8aa
Kitbomin <zzxxcc1433@naver.com>
2025-08-19 14:54:26 +0900

    Post 비즈니스로직 구현 -> repository 구현중

--- src/main/java/com/example/k5_iot_springboot/repository/D_PostRepository.java
+++ src/main/java/com/example/k5_iot_springboot/repository/D_PostRepository.java
@@ -4,6 +4,40 @@ import com.example.k5_iot_springboot.entity.D_Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+* Post 와 Comment의 관계가 1:N 의 관계
+*
+* D_Post post = postRepository.findById(id).get(); << 보통은 이렇게 많이 씀
+* post.getComments.forEach(...);  << 댓글 접근
+*
+* == 코드 풀이 ==
+* 1) 첫번째 쿼리: SELECT * FROM posts WHERE id = ?
+*
+* 2) 두번째 쿼리: LAZY 설정 코드를 "여버 번" 실행할 때마다 초기화를 위한 SELECT 문이 별도로 실행됨 -> 메모리가 아야해요
+*
+* ## 상황 1) 단일 Post만 조회하는 경우 ##
+* -- 1번째 쿼리
+*   SELECT * FROM posts WHERE id=?
+* -- 2번째 쿼리
+*   : 이후 post.getComments() 처음 호출 시 댓글 컬렉션 초기화용으로 딱 1번 실행됨
+*   SELECT * FROM comments where post_id=?
+*
+* ## 상황 2) Post를 N개 먼저 가져온 뒤 각 Post 마다 getComments() 호출을 하는 경우 ##
+* -- 1번째 쿼리
+*   SELECT * FROM posts limit 20;
+* -- 2번째 쿼리
+*   SELECT * FROM comments where post_id=?
+*       >> 총 20번 실행을 하게 됨
+*
+* 1번째 쿼리(1) + 2번째 쿼리(N)
+*   >> 1+N 쿼리 실행 문제 발생 
+*
+* */
+
+
 @Repository
 public interface D_PostRepository extends JpaRepository<D_Post, Long> {
+    // 게시글 조회 + 댓글까지 즉시 로딩
+
+
 }
--- src/main/java/com/example/k5_iot_springboot/service/D_CommentService.java
+++ src/main/java/com/example/k5_iot_springboot/service/D_CommentService.java
@@ -1,4 +1,16 @@
 package com.example.k5_iot_springboot.service;
 
+import com.example.k5_iot_springboot.dto.D_Comment.request.CommentCreateRequestDto;
+import com.example.k5_iot_springboot.dto.D_Comment.request.CommentUpdateRequestDto;
+import com.example.k5_iot_springboot.dto.D_Comment.response.CommentResponseDto;
+import com.example.k5_iot_springboot.dto.ResponseDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+
 public interface D_CommentService {
+    ResponseDto<CommentResponseDto> createComment(@Positive(message = "postID는 1 이상의 정수여야합니다.") Long postId, @Valid CommentCreateRequestDto dto);
+
+    ResponseDto<CommentResponseDto> updateComment(@Positive(message = "postID는 1 이상의 정수여야합니다.") Long postId, @Positive(message = "commentId는 1 이상의 정수여야합니다.") Long commentId, @Valid CommentUpdateRequestDto dto);
+
+    ResponseDto<CommentResponseDto> deleteComment(@Positive(message = "postID는 1 이상의 정수여야합니다.") Long postId, @Positive(message = "commentId는 1 이상의 정수여야합니다.") Long commentId);
 }
