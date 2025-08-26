63a78b144066bc7c395c0a39329c244daa193e5b
Kitbomin <zzxxcc1433@naver.com>
2025-08-26 10:50:23 +0900

    github에 이미 올라간 파일 제거하는 방법

---
+++ src/main/java/com/example/k5_iot_springboot/이론/_gitignore.java
@@ -0,0 +1,17 @@
+package com.example.k5_iot_springboot.이론;
+
+// github에 이미 올라간 파일 제거하는 방법
+
+// 1) Git 추적에서만 제거 (파일은 로컬에 유지)
+// : --cashed 옵션(깃에서만 제거, 로컬에선 유지됨)
+
+// git rm --cashed application-secret.properties
+
+
+// 2) commit & push 해야함
+
+// git commit -m "Remove application-secret.properties from repository"
+// git push origin main
+
+public class _gitignore {
+}
