02829aa6113d5010ef5873c0ba41dba34f4b6577
Kitbomin <zzxxcc1433@naver.com>
2025-08-27 16:36:22 +0900

    Admin 전용 서비스 구현

--- src/main/java/com/example/k5_iot_springboot/controller/G_AdminController.java
+++ src/main/java/com/example/k5_iot_springboot/controller/G_AdminController.java
@@ -1,5 +1,7 @@
 package com.example.k5_iot_springboot.controller;
 
+import com.example.k5_iot_springboot.dto.G_Admin.request.RoleManageRequest;
+import com.example.k5_iot_springboot.dto.G_Admin.response.RoleManageResponse;
 import com.example.k5_iot_springboot.dto.G_Auth.response.SignInResponse;
 import com.example.k5_iot_springboot.dto.G_User.request.RoleModifyRequest;
 import com.example.k5_iot_springboot.dto.ResponseDto;
@@ -31,12 +33,12 @@ public class G_AdminController {
     @PutMapping("/roles/replace")
     // 자원의 상태를 통째로 교체(덮어쓰기) => 동일한 요청을 여러번 보내도 결과가 같게끔
     // : 멱등성
-    public ResponseEntity<ResponseDto<Void>> replaceRoles(
+    public ResponseEntity<ResponseDto<RoleManageResponse.UpdateRolesResponse>> replaceRoles(
             @AuthenticationPrincipal UserPrincipal principal, //관리자 토큰 정보 가져오기
-            @Valid @RequestBody RoleModifyRequest req) {
-        ResponseDto<Void> response = adminService.replaceRoles(principal, req);
+            @Valid @RequestBody RoleManageRequest.UpdateRolesRequest req) {
+        ResponseDto<RoleManageResponse.UpdateRolesResponse> response = adminService.replaceRoles(principal, req);
 
-        return ResponseEntity.ok(ResponseDto.setSuccess("권한이 갱신되었습니다.", null));
+        return ResponseEntity.ok().body(response);
     }
 
 
@@ -44,10 +46,10 @@ public class G_AdminController {
     @PostMapping("/roles/add")
     // 새로운 자원을 추가 생성하거나, 기존 자원에 무언가를 덧붙임
     // : 요청을 여러번 보내면 결과가 달라질 수 있음
-    public ResponseEntity<ResponseDto<SignInResponse>> addRoles(
+    public ResponseEntity<ResponseDto<RoleManageResponse.AddRoleResponse>> addRoles(
             @AuthenticationPrincipal UserPrincipal principal, //관리자 토큰 정보 가져오기
-            @Valid @RequestBody RoleModifyRequest req) {
-        ResponseDto<SignInResponse> response = adminService.addRoles(principal, req);
+            @Valid @RequestBody RoleManageRequest.AddRoleRequest req) {
+        ResponseDto<RoleManageResponse.AddRoleResponse> response = adminService.addRole(principal, req);
 
         return ResponseEntity.ok().body(response);
     }
@@ -55,10 +57,10 @@ public class G_AdminController {
 
     // 3) 삭제
     @PostMapping("/roles/remove")
-    public ResponseEntity<ResponseDto<Void>> removeRoles(
+    public ResponseEntity<ResponseDto<RoleManageResponse.RemoveRoleResponse>> removeRoles(
             @AuthenticationPrincipal UserPrincipal principal, //관리자 토큰 정보 가져오기
-            @Valid @RequestBody RoleModifyRequest req) {
-        ResponseDto<Void> response = adminService.removeRoles(principal, req);
+            @Valid @RequestBody RoleManageRequest.RemoveRoleRequest req) {
+        ResponseDto<RoleManageResponse.RemoveRoleResponse> response = adminService.removeRole(principal, req);
 
         return ResponseEntity.ok(ResponseDto.setSuccess("권한이 삭제되었습니다.", null));
     }
--- src/main/java/com/example/k5_iot_springboot/service/impl/G_AdminServiceImpl.java
+++ src/main/java/com/example/k5_iot_springboot/service/impl/G_AdminServiceImpl.java
@@ -1,16 +1,20 @@
 package com.example.k5_iot_springboot.service.impl;
 
-import com.example.k5_iot_springboot.dto.G_Auth.response.SignInResponse;
-import com.example.k5_iot_springboot.dto.G_User.request.RoleModifyRequest;
+import com.example.k5_iot_springboot.common.enums.RoleType;
+import com.example.k5_iot_springboot.dto.G_Admin.request.RoleManageRequest;
+import com.example.k5_iot_springboot.dto.G_Admin.response.RoleManageResponse;
 import com.example.k5_iot_springboot.dto.ResponseDto;
 import com.example.k5_iot_springboot.entity.G_User;
 import com.example.k5_iot_springboot.repository.G_UserRepository;
 import com.example.k5_iot_springboot.security.UserPrincipal;
 import com.example.k5_iot_springboot.service.G_AdminService;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,42 +26,79 @@ public class G_AdminServiceImpl implements G_AdminService {
 
     @Override
     @Transactional
-    public ResponseDto<Void> replaceRoles(UserPrincipal principal, RoleModifyRequest req) {
+    public ResponseDto<RoleManageResponse.UpdateRolesResponse> replaceRoles(UserPrincipal principal, RoleManageRequest.@Valid UpdateRolesRequest req) {
 
-        return null;
-    }
+        // 1) 갱신당할 사용자 정보 조회
+        G_User user = userRepository.findWithRolesById(req.userId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 id의 유저가 없습니다.")) ;
 
+        // 2) 전체 교체 - 요청에 @NotEmpty 이므로 최소 1개 이상의 권한을 보장함
+        user.getRoles().clear();
+        req.roles().forEach(user::addRole);
 
-    @Override
-    @Transactional
-    public ResponseDto<SignInResponse> addRoles(UserPrincipal principal, RoleModifyRequest req) {
-        G_User user = userRepository.findByLoginId(principal.getUsername())
-                .orElseThrow(() -> new IllegalArgumentException("대상 사용자를 찾을 수 없습니다."));
+        // 갱신
+        userRepository.flush();
 
-        if (req.role() == null || user.getRoles().contains(req.role())) {
-            throw new IllegalArgumentException("추가할 권한이 없거나 이미 존재하는 권한입니다.");
-        }
-
-        user.getRoles().add(req.role());
-
-        SignInResponse response = new SignInResponse(
-                "",
-                "",
-                0,
+        RoleManageResponse.UpdateRolesResponse data = new RoleManageResponse.UpdateRolesResponse(
+                user.getId(),
                 user.getLoginId(),
-                user.getRoles().stream()
-                        .map(Enum::name)
-                        .collect(Collectors.toSet())
+
+                Set.copyOf(user.getRoles()),
+                // 복사해서 가져가는 이유 -> 방어적 복사
+                // -> JPA 엔티티 컬렉션 필드를 DTO 내부에서 조작할 경우 캡슐화 깨짐 위험 발생 -> 그래서 읽기 전용모드로 가져와서 씀
+
+                user.getUpdatedAt()
         );
 
-        return ResponseDto.setSuccess("권한이 추가되었습니다.", response);
+        return ResponseDto.setSuccess("SUCCESS", data);
     }
-
 
     @Override
     @Transactional
-    public ResponseDto<Void> removeRoles(UserPrincipal principal, RoleModifyRequest req) {
+    public ResponseDto<RoleManageResponse.AddRoleResponse> addRole(UserPrincipal principal, RoleManageRequest.@Valid AddRoleRequest req) {
+        G_User user = userRepository.findWithRolesById(req.userId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 id의 사용자가 없습니다."));
 
-        return null;
+        RoleType added = req.role();
+        user.addRole(added);
+
+        userRepository.flush(); //UpdatedAt 값이 바로 전달됨
+
+        RoleManageResponse.AddRoleResponse data = new RoleManageResponse.AddRoleResponse(
+                user.getId(),
+                user.getLoginId(),
+                added,
+                Set.copyOf(user.getRoles()),
+                user.getUpdatedAt()
+        );
+
+        return ResponseDto.setSuccess("SUCCESS", data);
+    }
+
+    @Override
+    @Transactional
+    public ResponseDto<RoleManageResponse.RemoveRoleResponse> removeRole(UserPrincipal principal, RoleManageRequest.@Valid RemoveRoleRequest req) {
+        G_User user = userRepository.findWithRolesById(req.userId())
+                .orElseThrow(() -> new EntityNotFoundException("해당 id의 사용자가 없습니다."));
+
+        RoleType removed = req.role();
+        user.removeRole(removed);
+
+        // 비워지는 경우 기본 USER 권한은 유지해야함 (최소 1개 이상의 권한을 가질 것을 보장하는 정책)
+        if (user.getRoles().isEmpty()) {
+            user.addRole(RoleType.USER);
+        }
+
+        userRepository.flush(); //UpdatedAt 값이 바로 전달됨
+
+        RoleManageResponse.RemoveRoleResponse data = new RoleManageResponse.RemoveRoleResponse(
+                user.getId(),
+                user.getLoginId(),
+                removed,
+                Set.copyOf(user.getRoles()),
+                user.getUpdatedAt()
+        );
+
+        return ResponseDto.setSuccess("SUCCESS", data);
     }
 }
