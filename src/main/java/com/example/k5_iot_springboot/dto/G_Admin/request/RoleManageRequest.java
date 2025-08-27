6716bbbd630620f4156f41e721a925927d088735
Kitbomin <zzxxcc1433@naver.com>
2025-08-27 15:41:24 +0900

    Security 권한 부여 및 권한별 접속 가능 기능 구현

---
+++ src/main/java/com/example/k5_iot_springboot/dto/G_Admin/request/RoleManageRequest.java
@@ -0,0 +1,9 @@
+package com.example.k5_iot_springboot.dto.G_Admin.request;
+
+public class RoleManageRequest {
+    public record UpdateRolesRequest() {}
+
+    public record AddRoleRequest() {}
+
+    public record RemoveRoleRequest() {}
+}
---
+++ src/main/java/com/example/k5_iot_springboot/service/G_UserService.java
@@ -0,0 +1,16 @@
+package com.example.k5_iot_springboot.service;
+
+import com.example.k5_iot_springboot.dto.G_User.request.UserProfileUpdateRequest;
+import com.example.k5_iot_springboot.dto.G_User.response.UserProfileResponse;
+import com.example.k5_iot_springboot.dto.ResponseDto;
+import com.example.k5_iot_springboot.security.UserPrincipal;
+import jakarta.validation.Valid;
+
+import java.nio.file.AccessDeniedException;
+
+public interface G_UserService {
+
+    ResponseDto<UserProfileResponse.MyPageResponse> getMyInfo(UserPrincipal principal);
+
+    ResponseDto<UserProfileResponse.MyPageResponse> updateMyInfo(UserPrincipal principal, @Valid UserProfileUpdateRequest request) ;
+}
